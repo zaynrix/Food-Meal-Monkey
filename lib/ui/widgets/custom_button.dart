3892ec0be191c668afa6988905783b5c5cbfd923
mac <zayns1914@gmail.com>
2023-08-12 16:17:52 +0200

    fix custom button

--- lib/ui/widgets/custom_button.dart
+++ lib/ui/widgets/custom_button.dart
@@ -1,38 +1,104 @@
 part of widgets;
+//
+// class CustomButton extends StatelessWidget {
+//   final String text;
+//   Function() onPress;
+//   final IconData? icon;
+//   final Color color;
+//
+//   CustomButton(
+//       {Key? key,
+//       required this.text,
+//       required this.onPress,
+//       this.icon,
+//       this.color = orangeColor})
+//       : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return ElevatedButton(
+//       onPressed: onPress,
+//       style: ButtonStyle(
+//         backgroundColor: MaterialStateProperty.all<Color>(color),
+//       ),
+//       child: icon == null
+//           ? Text(text)
+//           : Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Icon(icon),
+//                 SizedBox(
+//                   width: 20.w,
+//                 ),
+//                 Text(text)
+//               ],
+//             ),
+//     );
+//   }
+// }
 
-class CustomButton extends StatelessWidget {
+class CustomButton extends StatefulWidget {
   final String text;
-  Function() onPress;
+  final Function() onPress;
   final IconData? icon;
   final Color color;
 
-  CustomButton(
-      {Key? key,
-      required this.text,
-      required this.onPress,
-      this.icon,
-      this.color = orangeColor})
-      : super(key: key);
+  CustomButton({
+    Key? key,
+    required this.text,
+    required this.onPress,
+    this.icon,
+    this.color = orangeColor,
+  }) : super(key: key);
+
+  @override
+  _CustomButtonState createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onPress,
+      onPressed: _isLoading ? null : _onButtonPress,
       style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all<Color>(color),
+        backgroundColor: MaterialStateProperty.all<Color>(
+          _isLoading ? Colors.grey : widget.color,
+        ),
       ),
-      child: icon == null
-          ? Text(text)
-          : Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(icon),
-                SizedBox(
-                  width: 20.w,
-                ),
-                Text(text)
-              ],
-            ),
+      child: _isLoading
+          ? SizedBox(
+              width: 20,
+              height: 20,
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            )
+          : (widget.icon == null
+              ? Text(widget.text)
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(widget.icon),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(widget.text),
+                  ],
+                )),
     );
+  }
+
+  void _onButtonPress() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    await widget.onPress();
+
+    setState(() {
+      _isLoading = false;
+    });
   }
 }
