77674057ca02d8928a57088fb3ac09994faeb7d4
mac <zayns1914@gmail.com>
2023-08-12 17:59:52 +0200

    update

--- lib/routing/navigations.dart
+++ lib/routing/navigations.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-class ServiceNavigation{
 
+class ServiceNavigation {
   ServiceNavigation._();
   static ServiceNavigation serviceNavi = ServiceNavigation._();
   GlobalKey<NavigatorState> navKey = GlobalKey<NavigatorState>();
-
+  static final GlobalKey<ScaffoldMessengerState> scaffoldKey =
+      GlobalKey<ScaffoldMessengerState>();
   pushNamedWidget(String name) {
     navKey.currentState?.pushNamed(name);
   }
 
-  pushNamedReplacement(String name){
+  pushNamedReplacement(String name) {
     navKey.currentState?.pushReplacementNamed(name);
   }
 
@@ -17,10 +18,7 @@ class ServiceNavigation{
     navKey.currentState?.pop();
   }
 
-  pushNamedAndRemoveUtils(String name){
+  pushNamedAndRemoveUtils(String name) {
     navKey.currentState?.pushNamedAndRemoveUntil(name, (route) => false);
   }
-
-
-
 }
