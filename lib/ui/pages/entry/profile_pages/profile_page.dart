6d236d92b5073c777484c23d0cab5c78cacd3180
mac <zayns1914@gmail.com>
2023-08-12 16:18:29 +0200

    add _firestore

--- lib/ui/pages/entry/profile_pages/profile_page.dart
+++ lib/ui/pages/entry/profile_pages/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/routing/navigations.dart';
@@ -14,6 +15,7 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   TextEditingController nameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
@@ -22,7 +24,6 @@ class _ProfilePageState extends State<ProfilePage> {
   Future<void> _signOut() async {
     try {
       await _auth.signOut();
-
       ServiceNavigation.serviceNavi
           .pushNamedAndRemoveUtils(RouteGenerator.loginPage);
     } catch (e) {
@@ -34,10 +35,75 @@ class _ProfilePageState extends State<ProfilePage> {
   void initState() {
     super.initState();
     User? currentUser = _auth.currentUser;
+
     if (currentUser != null) {
-      nameController.text = currentUser.displayName ?? '';
-      emailController.text = currentUser.email ?? '';
-      mobileController.text = currentUser.phoneNumber ?? "";
+      print("User UID: ${currentUser.uid}");
+      print("User Phone Number: ${currentUser.phoneNumber}");
+
+      _firestore
+          .collection('users')
+          .doc(currentUser.uid)
+          .get()
+          .then((DocumentSnapshot docSnapshot) {
+        if (docSnapshot.exists) {
+          Map<String, dynamic> userData =
+              docSnapshot.data() as Map<String, dynamic>;
+          nameController.text = userData['name'] ?? '';
+          emailController.text = userData['email'] ?? '';
+          mobileController.text = userData['mobile'] ?? '';
+        }
+      }).catchError((error) {
+        print('Error retrieving user data: $error');
+      });
+    }
+  }
+
+  bool _isSaving = false; // Variable to track the saving state
+
+  Future<void> _updateProfile() async {
+    setState(() {
+      _isSaving = true; // Start saving state
+    });
+    try {
+      User? currentUser = _auth.currentUser;
+      if (currentUser != null) {
+        await currentUser.updateDisplayName(nameController.text);
+        await currentUser.updateEmail(emailController.text);
+        // Update user information in Firestore
+        await _firestore.collection('users').doc(currentUser.uid).update({
+          'name': nameController.text,
+          'email': emailController.text,
+          'mobile': mobileController.text,
+        });
+
+        // Show a success message or navigate to another screen
+        print('Profile updated successfully');
+      }
+    } catch (e) {
+      // Handle error
+      print('Error updating profile: $e');
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text(
+                'An error occurred while updating your profile. Please try again later.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } finally {
+      setState(() {
+        _isSaving = false; // End saving state
+      });
     }
   }
 
@@ -46,74 +112,70 @@ class _ProfilePageState extends State<ProfilePage> {
     User? currentUser = _auth.currentUser;
 
     return SingleChildScrollView(
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 20,
-            horizontal: 34,
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const CircleAvatar(
-                  maxRadius: 45,
-                  backgroundImage: AssetImage(
-                    'assets/images/user.png',
-                  ),
-                ),
-                const SizedBox(height: 8),
-                // const ColTxt(),
-                const SizedBox(height: 8),
-                Text(
-                  'Hi there ${currentUser?.displayName ?? 'User'}!',
-                  style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 2),
-                GestureDetector(
-                  onTap: _signOut,
-                  child: const Text(
-                    'Sign Out',
-                    style: TextStyle(color: Color(secondaryTextColor)),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                MainTextField(
-                  text: 'Name',
-                  type: TextInputType.text,
-                  controller: nameController,
-                ),
-                const SizedBox(height: 12),
-                MainTextField(
-                  text: 'Email',
-                  type: TextInputType.emailAddress,
-                  controller: emailController,
-                ),
-                const SizedBox(height: 12),
-                MainTextField(
-                  text: 'Mobile',
-                  type: TextInputType.phone,
-                  controller: mobileController,
-                ),
-                const SizedBox(height: 12),
-                const MainTextField(
-                  text: 'Password',
-                  type: TextInputType.emailAddress,
-                ),
-                const SizedBox(height: 12),
-                const MainTextField(
-                  text: 'Confirm Password',
-                  type: TextInputType.emailAddress,
-                ),
-                const SizedBox(height: 12),
-                CustomButton(
-                  text: 'Save',
-                  onPress: () {},
-                ),
-                const SizedBox(height: 50)
-              ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 34,
+        ),
+        child: Column(
+          children: [
+            const CircleAvatar(
+              maxRadius: 45,
+              backgroundImage: AssetImage(
+                'assets/images/user.png',
+              ),
             ),
-          ),
+            const SizedBox(height: 8),
+            const SizedBox(height: 8),
+            Text(
+              'Hi there ${currentUser?.displayName ?? 'User'}!',
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 2),
+            GestureDetector(
+              onTap: _signOut,
+              child: const Text(
+                'Sign Out',
+                style: TextStyle(color: Color(secondaryTextColor)),
+              ),
+            ),
+            const SizedBox(height: 20),
+            MainTextField(
+              text: 'Name',
+              type: TextInputType.text,
+              controller: nameController,
+            ),
+            const SizedBox(height: 12),
+            MainTextField(
+              text: 'Email',
+              type: TextInputType.emailAddress,
+              controller: emailController,
+            ),
+            const SizedBox(height: 12),
+            MainTextField(
+              text: 'Mobile',
+              type: TextInputType.phone,
+              controller: mobileController,
+            ),
+            const SizedBox(height: 12),
+            const MainTextField(
+              text: 'Password',
+              type: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 12),
+            const MainTextField(
+              text: 'Confirm Password',
+              type: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 12),
+            CustomButton(
+              text: _isSaving
+                  ? ''
+                  : 'Save', // Show text or empty string based on saving state
+              onPress: _updateProfile, // Disable button during saving
+            ),
+            const SizedBox(height: 50)
+          ],
         ),
       ),
     );
