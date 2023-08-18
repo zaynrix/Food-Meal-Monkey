9aaa864f078beba30cc3252712fbd05a06650816
Yahya Abunada <zayns1914@gmail.com>
2023-08-18 22:03:28 +0200

    fic chat logic

--- lib/ui/pages/entry/more_pages/chat/chat_controllers/chat_controller.dart
+++ lib/ui/pages/entry/more_pages/chat/chat_controllers/chat_controller.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+//PR
 import 'package:audioplayers/audioplayers.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
