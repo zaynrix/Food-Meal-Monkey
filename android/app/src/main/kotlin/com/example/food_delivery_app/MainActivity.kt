eafd1a45a8eae1183c4d0888d5628db48a586030
Yahya Abunada <zayns1914@gmail.com>
2024-01-21 20:31:24 +0100

    update package name

--- android/app/src/main/kotlin/com/example/food_delivery_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/food_delivery_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.food_delivery_app
+package com.techvalley.food_delivery
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
